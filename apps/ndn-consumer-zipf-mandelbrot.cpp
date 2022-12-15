d35c0be4fa35c83a703c12a5fa31669478ed78d3
Shohei Okubo <stream.larn3@gmail.com>
2022-12-15 22:13:22 +0900

    MandelBrotのSq数修正

--- apps/ndn-consumer-zipf-mandelbrot.cpp
+++ apps/ndn-consumer-zipf-mandelbrot.cpp
@@ -58,7 +58,7 @@ ConsumerZipfMandelbrot::GetTypeId(void)
 }
 
 ConsumerZipfMandelbrot::ConsumerZipfMandelbrot()
-  : m_N(100) // needed here to make sure when SetQ/SetS are called, there is a valid value of N
+  : m_N(10) // needed here to make sure when SetQ/SetS are called, there is a valid value of N
   , m_q(0.7)
   , m_s(0.7)
   , m_seqRng(CreateObject<UniformRandomVariable>())
